ceafa1351019baa4c8ad90d252e0d2912712643a
Yulevski <yonaiyuu@gmail.com>
2023-01-25 17:50:28 +0900

    addボタンでテキストが連携された画像追加

---
+++ Oekaki2/src/components/Main.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./Main.css";
+import pic from "./tree_green.png"; 
+//import {ReactMarkdown} from "react-markdown/lib/react-markdown";
+
+const Main = ({activeNote,notes}) => {
+  if(!activeNote){
+    return <div className='no-active-note'>no note is selected</div>
+  }
+
+  return (
+    <div className='app-main'>
+      <div className='app-main-note-edit'>
+        {/* <img src={pic} alt="picture" />
+        <img src={pic} alt="picture" />  */}
+        {notes.map((note)=>(
+               <img src={note.picturePath} alt="picture" key={note.id} /> 
+          
+            ))}       
+        {/* <input type="text"/>
+        <textarea id="" placeholder='INPUT NOTE'></textarea> */}
+      </div>
+      {/* <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <div className="markdown-preview">{activeNote.content}</div>
+      </div> */}
+    </div>
+  );
+};
+
+export default Main;
---
+++ Oekaki2/src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = ({onAddNote, notes, onDeleteNote,activeNote,setActiveNote}) => {
+    console.log("Snotes is ", notes);
+    console.log("activeNote is",activeNote);
+    console.log("setActiveNots is",setActiveNote);
+    console.log("notes is", notes);
+  return (
+    <div className="app-sidebar">
+        <div className="app-sidebar-header">
+            <h1>NOTE</h1>
+            <button onClick={onAddNote}>ADD</button>
+        </div>
+          
+        <div className="app-sidebar-notes">
+            {/* ↓notesの値をnoteへ */}
+            {notes.map((note)=>(
+                <div className={`app-sidebar-note ${note.id === activeNote && "active"}`} 
+                key={note.id} //noteのidを送る
+                onClick={()=> setActiveNote(note.id)}>
+
+                    <div className="sidebar-note-title">
+                        <strong>{note.title}</strong>
+                        <div>
+                            <input type="text"/>
+                            <textarea id="" placeholder='INPUT explanation'></textarea>
+                        </div>
+                        {/* arrow関数で書きリロードしたときに作動しないように,引数をとる時 */}
+                        <button onClick={()=>onDeleteNote(note.id)}>Delete</button>
+                    </div>
+                    <p>{note.content}</p>
+                    <small>{new Date(note.modDate).toLocaleDateString("ja-JP",{
+                        hour: "2-digit",
+                        minute:"2-digit",
+                    })}</small>
+                </div>
+
+            ))}
+           
+        </div>
+    </div>
+ 
+  );
+};
+
+export default Sidebar;
