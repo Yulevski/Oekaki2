b40f687c0d7c3dc179aa5d88e7a00e5354af88f3
Yulevski <yonaiyuu@gmail.com>
2023-02-02 16:37:39 +0900

    ライブラリ「react-draggable」によって画像をドラッグで移動可能に

--- Oekaki2/src/components/Main.jsx
+++ Oekaki2/src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Main.css";
 import pic from "./tree_green.png"; 
 //import {ReactMarkdown} from "react-markdown/lib/react-markdown";
+import Draggable from 'react-draggable';
 
 const Main = ({notes,onUpdateNote}) => {
 
@@ -12,21 +13,18 @@ const Main = ({notes,onUpdateNote}) => {
       picturePath:pic,
     });
   };
-  const interact =useInteractJS()
+ 
   return (
     <div className='app-main'>
-      <div className='app-main-note-edit'>
-        ref={interact.ref}
-        {notes.map((note)=>(
-               <img src={note.picturePath} alt="picture" key={note.id} style={{...interact.style}}
-                            onClick={()=>onEditNote(note.id)}
-               /> 
-          
-            ))}       
-     
-      </div>
-     
-    </div>
+      <Draggable defaultPosition={{x: 0, y: 0}}>
+        <div style={{ position: 'absolute' }} className='app-main-note-edit'>
+          <p>
+          {notes.map((note)=>(<img src={note.picturePath} alt="picture" key={note.id} onClick={()=>onEditNote(note.id)}/> 
+                        ))}  
+          </p>
+        </div>
+      </Draggable>
+     </div>
   );
 };
 
