f94ee84d529c155cc36b223f6586555de336ffa2
Yulevski <yonaiyuu@gmail.com>
2023-03-01 11:32:40 +0900

    image挿入可能に。objectとtextが同じ名前を持つように変更

--- Oekaki2/src/components/Main.jsx
+++ Oekaki2/src/components/Main.jsx
@@ -1,8 +1,9 @@
 // import React from 'react';
 // import "./Main.css";
-// import pic from "./tree_green.png"; 
 import React, { useState, useEffect } from "react";
 import { fabric } from "fabric";
+import pic from "./tree_green.png";
+
 const App = () => {
   const [canvas, setCanvas] = useState("");
   useEffect(() => {
@@ -17,6 +18,13 @@ const App = () => {
       if (clickedObject instanceof fabric.Rect) {
         console.log("Clicked rect name is", clickedObject.name);
       }
+      if (clickedObject instanceof fabric.Image) {
+        console.log("Clicked rect name is", clickedObject.name);
+      }
+      if (clickedObject instanceof fabric.Textbox) {
+        console.log("Clicked rect name is", clickedObject.name);
+      }
+
     });
 
     setCanvas(canvas);
@@ -30,16 +38,46 @@ const App = () => {
       height: 280,
       width: 200,
       fill: "yellow",
-      name: "rect-"+ Date.now()
+      name: "rect-" + Date.now(),
     });
     console.log("name is", rect.name);
     canvi.add(rect);
     canvi.renderAll();
   };
+
+  const addPic = (canvi) => {
+    fabric.Image.fromURL(pic, function(img) {
+      img.set({ name: "pic-" + Date.now() });
+      canvi.add(img);
+    });
+  };
+
+  const addText = (canvi) => {
+    const activeObject = canvi.getActiveObject();
+    if (activeObject instanceof fabric.Rect || activeObject instanceof fabric.Image) {
+      const textBox = new fabric.Textbox("", {
+        name: activeObject.name,
+        left: activeObject.left + 20,
+        top: activeObject.top + 20,
+        width: 200,
+        fontSize: 18,
+        fontFamily: "Arial",
+        fill: "#000000",
+      });
+      canvi.add(textBox);
+      canvi.setActiveObject(textBox);
+      textBox.enterEditing();
+      textBox.hiddenTextarea.focus();
+    }
+  };
+  
+  
   return (
     <div>
-      <h1>Fabric.js </h1>
+      <h1>Fabric.js</h1>
       <button onClick={() => addRect(canvas)}>Rectangle</button>
+      <button onClick={() => addPic(canvas)}>Add Pic</button>
+      <button onClick={() => addText(canvas)}>Add Text</button>
       <button onClick={() => console.log(canvas.getActiveObject())}>
         Test
       </button>
@@ -47,4 +85,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
