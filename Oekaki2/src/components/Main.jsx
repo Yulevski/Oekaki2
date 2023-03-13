e9878a7cac08a364b985b0a23783e141c47bcc5e
hiro-iku <hiroaki0613@gmail.com>
2023-03-13 17:33:44 +0900

    fabric.util.createClassを使ったサンプル

--- Oekaki2/src/components/Main.jsx
+++ Oekaki2/src/components/Main.jsx
@@ -1,16 +1,19 @@
-
 // import React from 'react';
 // import "./Main.css";
 import React, { useState, useEffect } from "react";
 import { fabric } from "fabric";
 import pic from "./tree_green.png";
 
-
 const App = () => {
   const [canvas1, setCanvas1] = useState("");
   const [textbox1, setTextbox1] = useState("");
   // const [canvas2, setCanvas2] = useState("");
-  
+  const [texts, setTexts] = useState({
+    descriptionColor: "",
+    descriptionPosition: "",
+  });
+  const [clickedObject, setClickedObject] = useState(null);
+
   useEffect(() => {
     const canvas1 = new fabric.Canvas("canvas1", {
       height: 800,
@@ -21,7 +24,15 @@ const App = () => {
     canvas1.on("mouse:down", (options) => {
       const clickedObject = options.target;
       if (clickedObject instanceof fabric.Rect) {
-        console.log("Clicked rect name is", clickedObject.name);
+        console.log(
+          "Clicked rect name is",
+          clickedObject.texts.descriptionColor
+        );
+        setClickedObject(clickedObject);
+        setTexts({
+          descriptionColor: clickedObject.texts.descriptionColor,
+          descriptionPosition: clickedObject.texts.descriptionPosition,
+        });
       }
       if (clickedObject instanceof fabric.Image) {
         console.log("Clicked rect name is", clickedObject.name);
@@ -37,9 +48,8 @@ const App = () => {
     };
   }, []);
 
-
   const addRect = (canvi) => {
-    const rect = new fabric.Rect({
+    const rect = new RectWithText({
       height: 280,
       width: 200,
       fill: "yellow",
@@ -47,48 +57,22 @@ const App = () => {
     });
     console.log("name is", rect.name);
     canvi.add(rect);
-    
-    const textBox = new fabric.Textbox("", {
-      name: rect.name,
-      left: rect.left + 20,
-      top: rect.top + 20,
-      width: 200,
-      fontSize: 18,
-      fontFamily: "Arial",
-      fill: "#111111",
-      backgroundColor: "#CCCCCC",
-    });
-    canvi.add(textBox);
-    canvi.renderAll();
+    //canvi.renderAll();
   };
 
   const addPic = (canvi) => {
-    fabric.Image.fromURL(pic, function(img) {
+    fabric.Image.fromURL(pic, function (img) {
       img.set({ name: "pic-" + Date.now() });
       canvi.add(img);
     });
   };
 
-  // const addText = (canvi) => {
-  //   const activeObject = canvas1.getActiveObject();
-  //   if (activeObject instanceof fabric.Rect || activeObject instanceof fabric.Image) {
-  //     const textBox = new fabric.Textbox("", {
-  //       name: activeObject.name,
-  //       left: activeObject.left + 20,
-  //       top: activeObject.top + 20,
-  //       width: 200,
-  //       fontSize: 18,
-  //       fontFamily: "Arial",
-  //       fill: "#111111",
-  //       backgroundColor: "#CCCCCC",
-  //     });
-  //     canvi.add(textBox);
-  //     canvi.setActiveObject(textBox);
-  //     textBox.enterEditing();
-  //     textBox.hiddenTextarea.focus();
-  //   }
-  // };
-  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTexts({ ...{ name: value } });
+    clickedObject.setText(name, e.target.value);
+  };
+
   return (
     <div style={{ display: "flex" }}>
       <div>
@@ -103,10 +87,52 @@ const App = () => {
 
       <div className="textBox">
         <h1>Textbox Contents</h1>
-        <input type="text" />
+        <div hidden={clickedObject === null ? true : false}>
+          <div>
+            <h3>色・グラデーションの説明</h3>
+            <textarea
+              name="descriptionColor"
+              value={texts.descriptionColor}
+              placeholder="感情との関係はなんだろう。理由は？"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <h3>配置・向きの説明</h3>
+            <textarea
+              name="descriptionPosition"
+              value={texts.descriptionPosition}
+              placeholder="感情との関係はなんだろう。理由は？"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
 };
+
+var RectWithText = fabric.util.createClass(fabric.Rect, {
+  // コンストラクタでtextsプロパティを追加する
+  initialize: function (options) {
+    this.callSuper("initialize", options);
+    this.set("texts", { descriptionColor: "", descriptionPosition: "" });
+  },
+
+  // toObjectメソッドでtextプロパティをJSONに追加する
+  toObject: function () {
+    return fabric.util.object.extend(this.callSuper("toObject"), {
+      text: this.get("text"),
+    });
+  },
+
+  // setTextメソッドでtextsプロパティを更新する
+  setText: function (key, text) {
+    var texts = this.get("texts");
+    texts[key] = text;
+    this.set("texts", texts);
+    this.canvas.renderAll();
+  },
+});
 
 export default App;
