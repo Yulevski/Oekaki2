1211888d2fa827acfd051cca79bb53aec84af27e
Yulevski <yonaiyuu@gmail.com>
2023-05-26 10:55:45 +0900

    ファイル名やディレクトリ整理

--- Oekaki2/src/App.jsx
+++ Oekaki2/src/App.jsx
@@ -1,39 +1,11 @@
 import { useState } from 'react'
-// import './App.css';
 import Main from './components/Main';
-// import Sidebar from './components/Sidebar';
-// import uuid from "react-uuid";
-// import pic from "./components/tree_green.png"; 
 
 function App() {
-  //ノートを追加する為配列として保持
-  //notesが複数のノートを保持 setNotesでnotes更新
-  const [notes, setNotes] = useState([]);
-
-
-    const onUpdateNote=(updatedNote)=>{
-      //新しいノートの配列を返す
-      const updatedNotesArray=notes.map((note)=>{
-        if(note.id===updatedNote.id){
-          return updatedNote;
-        } else{
-          return note;
-        }
-      });
-      setNotes(updatedNotesArray);
-    };
-
+  
   return (
     <div className="App">
-        {/* <Sidebar 
-        onAddNote={onAddNote} 
-        notes={notes} 
-        onDeleteNote={onDeleteNote}
-        /> */}
-        <Main
-        notes={notes} 
-        onUpdateNote={onUpdateNote}
-        />
+        <Main/>
     </div>
   )
 }
--- Oekaki2/src/main2.jsx
+++ Oekaki2/src/main2.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-// import Main from './src/Main.jsx'
+// import App from './App'
+import Main from './components/Main'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Main />
+    {/* <App /> */}
   </React.StrictMode>,
 )
