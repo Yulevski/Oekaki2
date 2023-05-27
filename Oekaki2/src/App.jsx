eeb1696a926923f84b384ea6f2881c9829f952d3
Yulevski <yonaiyuu@gmail.com>
2023-05-27 14:58:43 +0900

    app.jsxでルーティング

--- Oekaki2/src/App.jsx
+++ Oekaki2/src/App.jsx
@@ -1,5 +1,52 @@
 import { useState } from 'react'
 import Main from './components/Main';
+import { BrowserRouter, Route,Switch ,Link,NavLink} from 'react-router-dom';
+
+function Page() {
+  
+  return (
+    <BrowserRouter>
+    <h1>Hello React Router</h1>
+    <ul>
+        <li>
+          <NavLink activeStyle={{
+                fontWeight: 'bold',
+                color: 'red',
+              }} activeClassName="active" exact to="/">
+            <Link to="/">Theme</Link>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/onomatope">
+          <Link to="/onomatope">Onomatope</Link>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/app">
+          <Link to="/app">App</Link>
+          </NavLink>
+        </li>
+    </ul>
+    <Switch>
+      <Route exact path="/" component={Theme}>
+      </Route>
+      <Route path="/onomatope" component={Onomatope}>
+      </Route>
+      <Route path="/app" component={App}>
+      </Route>
+    </Switch>
+  </BrowserRouter>
+  );
+}
+
+
+function Theme() {
+  return <h2>Theme</h2>;
+}
+
+function Onomatope() {
+  return <h2>Onomatope</h2>;
+}
 
 function App() {
   
@@ -10,4 +57,4 @@ function App() {
   )
 }
 
-export default App;
+export default Page;
