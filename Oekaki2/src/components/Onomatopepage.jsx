3e23c2a0cded9fd09981980c17f72c24c179f9fe
Yulevski <yonaiyuu@gmail.com>
2023-05-31 18:52:56 +0900

    valueをボタンで設定

--- Oekaki2/src/components/Onomatopepage.jsx
+++ Oekaki2/src/components/Onomatopepage.jsx
@@ -16,7 +16,13 @@ function Onomatope({themeValue,onomatopeValue, setOnomatopeValue}) {
       </NavLink>
     </li>
       <h2>Here is Onomatope {themeValue}</h2>
-      <textarea value={onomatopeValue} onChange={handleChange} />
+      {/* <textarea value={onomatopeValue} onChange={handleChange} /> */}
+    <button onClick={() => setOnomatopeValue('Value 1')}>Button 1</button>
+    <button onClick={() => setOnomatopeValue('Value 2')}>Button 2</button>
+    <button onClick={() => setOnomatopeValue('Value 3')}>Button 3</button>
+    <button onClick={() => setOnomatopeValue('Value 4')}>Button 4</button>
+    <button onClick={() => setOnomatopeValue('Value 5')}>Button 5</button>
+    <button onClick={() => setOnomatopeValue('Value 6')}>Button 6</button>
 
     </>
   );
--- Oekaki2/src/components/Themepage.jsx
+++ Oekaki2/src/components/Themepage.jsx
@@ -19,9 +19,16 @@ function Themepage({themeValue,setThemeValue }) {
       </NavLink>
     </li>
     <h2>Here is Theme in themepage</h2>
-    <textarea value={themeValue} onChange={handleChange} />
-    
+    {/* <textarea value={themeValue} onChange={handleChange} /> */}
+    {/* Six buttons with assigned values */}
+    <button onClick={() => setThemeValue('Value 1')}>Button 1</button>
+    <button onClick={() => setThemeValue('Value 2')}>Button 2</button>
+    <button onClick={() => setThemeValue('Value 3')}>Button 3</button>
+    <button onClick={() => setThemeValue('Value 4')}>Button 4</button>
+    <button onClick={() => setThemeValue('Value 5')}>Button 5</button>
+    <button onClick={() => setThemeValue('Value 6')}>Button 6</button>
     </div>
+    
     
   )
 }
